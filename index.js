c483e1701f5642bd9b687a571fa821a5003ea3e3
raybon254 <114833250+raybon254@users.noreply.github.com>
2025-03-17 00:24:42 +0300

    Update index.js reduce lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
-const totalBatteries=assembledBatteries.reduce((sum ,battery)=>sum + battery, 0)
+const totalBatteries=batteryBatches.reduce((sum ,battery)=>sum + battery, 0)
    
 module.exports={totalBatteries};
